4ae46bd01cf42f7f5906351cc0ea09c1e5db76ba
Owen Schwartz <oschwartz10612@gmail.com>
2019-03-25 20:36:11 -0400

    forms

--- README.md
+++ README.md
@@ -4,15 +4,30 @@ Auth is a Node JS template for authenticating users.
 
 ## MYSQL
 
-```python
+```bash
+docker run --name mysql -v /Users/owenschwartz/Desktop/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.6
+```
+
+```mysql
+docker exec -it mysql bash
+
 create database users;
 
-use database users;
+use users;
 
-create table google ;
+create table google (google_id varchar(50), name varchar(50), user_id varchar(100));
 
-create table google (user_id INT AUTO_INCREMENT PRIMARY KEY, name varchar(50), google_id varchar(50));
+create table data1 (text varchar(50), textarea MEDIUMTEXT, dropdown varchar(20), user_id varchar(100));
 
+SELECT 
+    *
+FROM
+    data1
+INTO OUTFILE '/var/lib/mysql-files/data.csv'
+FIELDS ENCLOSED BY '"'
+TERMINATED BY ';'
+ESCAPED BY '"'
+LINES TERMINATED BY '\r\n';
 ```
 
 ## Contributing
--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const express = require('express');
+const mysql = require('../config/mysql');
 
-const authCheck = function (req, res, next) {
+const authCheck = function(req, res, next) {
   if (!req.user) {
     res.redirect('/auth/login');
   } else {
@@ -8,8 +10,55 @@ const authCheck = function (req, res, next) {
   }
 };
 
-router.get('/', authCheck, function (req, res) {
-  res.render('pages/profile', {user: req.user});
+router.get('/', authCheck, function(req, res) {
+  let sql = `SELECT * FROM data1 WHERE user_id = ?`;
+  let query = mysql.query(sql, req.user.user_id, (err, result) => {
+      if (err) throw err;
+      if (result[0] != null) {
+        res.render('pages/profile', {
+          user: req.user,
+          data: {
+            text: result[0].text,
+            textarea: result[0].textarea,
+            dropdown: result[0].dropdown
+          }
+        });
+      } else {
+        res.render('pages/profile', {
+          user: req.user,
+          data: {
+            text: null,
+            textarea: null,
+            dropdown: null
+          }
+        });
+    }
+  });
+});
+
+router.post('/form', express.urlencoded({ extended: true }), function(req, res) {
+  let sql = `SELECT * FROM data1 WHERE user_id = ?`;
+  let query = mysql.query(sql, req.user.user_id, (err, result) => {
+  if (err) throw err;
+  if (result[0] != null) {
+    let sql = `UPDATE data1 SET text = ?, textarea = ?, dropdown = ? WHERE user_id = ?`;
+    let query = mysql.query(sql, [req.body.text, req.body.textarea, req.body.select, req.user.user_id], (err, result) => {
+      if (err) throw err;
+    });
+  } else {
+    let data = {
+      user_id: req.user.user_id,
+      text: req.body.text,
+      textarea: req.body.textarea,
+      dropdown: req.body.select
+    };
+    let sql = 'INSERT INTO data1 SET ?';
+    let query = mysql.query(sql, data, (err, result) => {
+      if (err) throw err;
+    });
+  }
+  });
+  res.redirect('back');
 });
 
 module.exports = router;
