ea9d0767d1c275d2595b9c0d333868e17d9a11e9
Owen Schwartz <oschwartz10612@gmail.com>
2019-04-04 19:51:00 -0400

    bug fixes and added text

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -7,20 +7,30 @@ const authCheck = function(req, res, next) {
     res.redirect('/home?login=y');
   } else if (req.user.role == 'admin') {
     next();
-  }
-  else {
+  } else {
     res.redirect('/profile');
   }
 };
 
 router.get('/', authCheck, function(req, res) {
-  let sql = `SELECT * FROM data`;
+  let sql = `SELECT * FROM team`;
   let query = mysql.query(sql, req.user.user_id, (err, result) => {
-      if (err) throw err;
-      if (result[0] != null) {
-        res.render('pages/admin', {user: req.user, data: result[0]});
+    if (err) throw err;
+    if (result[0] != null) {
+      var payload = {
+        user: req.user,
+        team: result
       }
-    });
+      let sql = `SELECT * FROM officers`;
+      let query = mysql.query(sql, req.user.user_id, (err, result) => {
+        if (err) throw err;
+        if (result[0] != null) {
+          payload.officers = result;
+          res.render('pages/admin', payload);
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
