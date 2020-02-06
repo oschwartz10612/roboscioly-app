ab739bf3c4e18a854c7d8f99aec39f859308ba5f
Owen Schwartz <oschwartz10612@gmail.com>
2020-02-06 07:03:18 -0500

    Editable tables in admin
    For app submissions
    For teacher entries

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const mysql = require('../config/mysql');
+const express = require('express');
 
 const authCheck = function(req, res, next) {
   if (!req.user) {
@@ -37,6 +38,19 @@ router.get('/', authCheck, function(req, res) {
 
 router.get('/api/alldata', authCheck, function(req, res) {
   let sql = `SELECT * FROM team`;
+  mysql.query(sql, (err, result) => {
+    if (err) throw err;
+    if (result[0] != null) {
+      var resultJson = JSON.stringify(result);
+      resultJson = JSON.parse(resultJson);
+
+      res.send(resultJson);
+    }
+  });
+});
+
+router.get('/api/teacher_data', authCheck, function(req, res) {
+  let sql = `SELECT * FROM teachers`;
   mysql.query(sql, (err, result) => {
     if (err) throw err;
     if (result[0] != null) {
@@ -87,6 +101,90 @@ router.get('/api/openCollectEmail', authCheck, function(req, res) {
   mysql.query(sql, (err) => { if (err) throw err; });
   global.collectEmail = 'yes';
   res.send('done');
+});
+
+router.post('/api/update_sql', authCheck, express.urlencoded({ extended: true }), function(req, res) {
+  const table = "team";
+  const user_id = req.body.user_id;
+  delete req.body.user_id;
+  const values = Object.values(req.body);
+  const keys = Object.keys(req.body);
+
+  //Update table
+  let sql = 'SELECT * FROM ' + table + ' WHERE user_id = ?';
+  mysql.query(sql, user_id, (err, result) => {
+  if (err) throw err;
+  if (result[0] != null) {
+
+    let sql = 'UPDATE ' + table + ' SET ';
+    for (var i = 0; i < keys.length - 1; i++) {
+      sql = sql + keys[i] + ' = ?, '
+    }
+    sql = sql + keys[keys.length-1] + ' = ? WHERE user_id = ?';
+
+    var data = values;
+    data.push(user_id);
+
+    mysql.query(sql, data, (err) => {
+      if (err) throw err;
+    });
+  } else {
+
+    //new submittion
+    let submittion = req.body;
+    submittion.user_id = user_id;
+
+    let sql = 'INSERT INTO ' + table + ' SET ?';
+    mysql.query(sql, submittion, (err) => {
+      if (err) throw err;
+    });
+  }
+  });
+
+  res.json({success : "Updated Successfully", status : 200});
+});
+
+router.post('/api/update_sql_teachers', authCheck, express.urlencoded({ extended: true }), function(req, res) {
+  const table = "teachers";
+  const id = req.body.id;
+  delete req.body.id;
+  const values = Object.values(req.body);
+  const keys = Object.keys(req.body);
+
+  //Update table
+  if (id !== "new") {
+
+  let sql = 'SELECT * FROM ' + table + ' WHERE ID = ?';
+  mysql.query(sql, id, (err, result) => {
+  if (err) throw err;
+  if (result[0] != null) { }
+
+    let sql = 'UPDATE ' + table + ' SET ';
+    for (var i = 0; i < keys.length - 1; i++) {
+      sql = sql + keys[i] + ' = ?, '
+    }
+    sql = sql + keys[keys.length-1] + ' = ? WHERE ID = ?';
+
+    var data = values;
+    data.push(id);
+
+    mysql.query(sql, data, (err) => {
+      if (err) throw err;
+    });
+  });
+
+  } else {
+
+    //new submittion
+    let submittion = req.body;
+    let sql = 'INSERT INTO ' + table + ' SET ?';
+    mysql.query(sql, submittion, (err) => {
+      if (err) throw err;
+    });
+  }
+
+
+  res.json({success : "Updated Successfully", status : 200});
 });
 
 module.exports = router;
